fbb936b09760a203428464c0450d884dbf6c943b
LeandroRufino <leandroh.rufinods@gmail.com>
2024-10-21 15:43:37 -0300

    agora

---
+++ WindowsFormsControleFuncionários/Esqueceu sua senha.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace WindowsFormsControleFuncionários
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFechar = new System.Windows.Forms.Button();
+            this.pnlEsqueceu = new System.Windows.Forms.Panel();
+            this.pbxVoltar = new System.Windows.Forms.PictureBox();
+            this.btnMinimizar = new System.Windows.Forms.Button();
+            this.btnMaximizar = new System.Windows.Forms.Button();
+            this.lblSenha = new System.Windows.Forms.Label();
+            this.lblEsqueceuSua = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblCodigo = new System.Windows.Forms.Label();
+            this.btnConfirmar = new System.Windows.Forms.Button();
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.tbxCodigoInserir = new System.Windows.Forms.TextBox();
+            this.lblCodigoInserir = new System.Windows.Forms.Label();
+            this.tbxEmailCadastrado = new System.Windows.Forms.TextBox();
+            this.lblEmailCadastrado = new System.Windows.Forms.Label();
+            this.pnlEsqueceu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxVoltar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnFechar.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFechar.Location = new System.Drawing.Point(969, 4);
+            this.btnFechar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(33, 31);
+            this.btnFechar.TabIndex = 2;
+            this.btnFechar.Text = "X";
+            this.btnFechar.UseVisualStyleBackColor = true;
+            // 
+            // pnlEsqueceu
+            // 
+            this.pnlEsqueceu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(40)))), ((int)(((byte)(49)))));
+            this.pnlEsqueceu.Controls.Add(this.pbxVoltar);
+            this.pnlEsqueceu.Controls.Add(this.btnMinimizar);
+            this.pnlEsqueceu.Controls.Add(this.btnMaximizar);
+            this.pnlEsqueceu.Controls.Add(this.btnFechar);
+            this.pnlEsqueceu.Controls.Add(this.lblSenha);
+            this.pnlEsqueceu.Controls.Add(this.lblEsqueceuSua);
+            this.pnlEsqueceu.Cursor = System.Windows.Forms.Cursors.Default;
+            this.pnlEsqueceu.Location = new System.Drawing.Point(146, 10);
+            this.pnlEsqueceu.Margin = new System.Windows.Forms.Padding(4);
+            this.pnlEsqueceu.Name = "pnlEsqueceu";
+            this.pnlEsqueceu.Size = new System.Drawing.Size(1000, 217);
+            this.pnlEsqueceu.TabIndex = 24;
+            // 
+            // pbxVoltar
+            // 
+            this.pbxVoltar.Location = new System.Drawing.Point(19, 16);
+            this.pbxVoltar.Margin = new System.Windows.Forms.Padding(4);
+            this.pbxVoltar.Name = "pbxVoltar";
+            this.pbxVoltar.Size = new System.Drawing.Size(133, 62);
+            this.pbxVoltar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbxVoltar.TabIndex = 5;
+            this.pbxVoltar.TabStop = false;
+            // 
+            // btnMinimizar
+            // 
+            this.btnMinimizar.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnMinimizar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnMinimizar.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMinimizar.Location = new System.Drawing.Point(907, 4);
+            this.btnMinimizar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnMinimizar.Name = "btnMinimizar";
+            this.btnMinimizar.Size = new System.Drawing.Size(33, 31);
+            this.btnMinimizar.TabIndex = 4;
+            this.btnMinimizar.UseVisualStyleBackColor = true;
+            // 
+            // btnMaximizar
+            // 
+            this.btnMaximizar.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnMaximizar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnMaximizar.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMaximizar.Location = new System.Drawing.Point(939, 4);
+            this.btnMaximizar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnMaximizar.Name = "btnMaximizar";
+            this.btnMaximizar.Size = new System.Drawing.Size(33, 31);
+            this.btnMaximizar.TabIndex = 3;
+            this.btnMaximizar.UseVisualStyleBackColor = true;
+            // 
+            // lblSenha
+            // 
+            this.lblSenha.AutoSize = true;
+            this.lblSenha.BackColor = System.Drawing.Color.Transparent;
+            this.lblSenha.Font = new System.Drawing.Font("Segoe UI", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSenha.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblSenha.Location = new System.Drawing.Point(399, 111);
+            this.lblSenha.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblSenha.Name = "lblSenha";
+            this.lblSenha.Size = new System.Drawing.Size(154, 62);
+            this.lblSenha.TabIndex = 1;
+            this.lblSenha.Text = "senha";
+            // 
+            // lblEsqueceuSua
+            // 
+            this.lblEsqueceuSua.AutoSize = true;
+            this.lblEsqueceuSua.BackColor = System.Drawing.Color.Transparent;
+            this.lblEsqueceuSua.Cursor = System.Windows.Forms.Cursors.Default;
+            this.lblEsqueceuSua.Font = new System.Drawing.Font("Segoe UI", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEsqueceuSua.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblEsqueceuSua.Location = new System.Drawing.Point(315, 28);
+            this.lblEsqueceuSua.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblEsqueceuSua.Name = "lblEsqueceuSua";
+            this.lblEsqueceuSua.Size = new System.Drawing.Size(318, 62);
+            this.lblEsqueceuSua.TabIndex = 0;
+            this.lblEsqueceuSua.Text = "Esqueceu sua";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmail.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.lblEmail.Location = new System.Drawing.Point(307, 550);
+            this.lblEmail.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(229, 92);
+            this.lblEmail.TabIndex = 32;
+            this.lblEmail.Text = "Após inserir seu email,\r\nvocê receberá um código\r\nde 6 digitos para redefirnir\r\ns" +
+    "ua senha";
+            // 
+            // lblCodigo
+            // 
+            this.lblCodigo.AutoSize = true;
+            this.lblCodigo.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCodigo.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.lblCodigo.Location = new System.Drawing.Point(807, 563);
+            this.lblCodigo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblCodigo.Name = "lblCodigo";
+            this.lblCodigo.Size = new System.Drawing.Size(234, 46);
+            this.lblCodigo.TabIndex = 31;
+            this.lblCodigo.Text = "Insira o código de 6 dígitos \r\npara redefinir sua senha";
+            // 
+            // btnConfirmar
+            // 
+            this.btnConfirmar.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btnConfirmar.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConfirmar.Location = new System.Drawing.Point(813, 417);
+            this.btnConfirmar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnConfirmar.Name = "btnConfirmar";
+            this.btnConfirmar.Size = new System.Drawing.Size(240, 43);
+            this.btnConfirmar.TabIndex = 30;
+            this.btnConfirmar.Text = "Confirmar código";
+            this.btnConfirmar.UseVisualStyleBackColor = true;
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btnEnviar.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEnviar.Location = new System.Drawing.Point(299, 417);
+            this.btnEnviar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(240, 43);
+            this.btnEnviar.TabIndex = 29;
+            this.btnEnviar.Text = "Enviar código";
+            this.btnEnviar.UseVisualStyleBackColor = true;
+            // 
+            // tbxCodigoInserir
+            // 
+            this.tbxCodigoInserir.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.tbxCodigoInserir.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbxCodigoInserir.Location = new System.Drawing.Point(691, 357);
+            this.tbxCodigoInserir.Margin = new System.Windows.Forms.Padding(4);
+            this.tbxCodigoInserir.Multiline = true;
+            this.tbxCodigoInserir.Name = "tbxCodigoInserir";
+            this.tbxCodigoInserir.Size = new System.Drawing.Size(459, 35);
+            this.tbxCodigoInserir.TabIndex = 28;
+            // 
+            // lblCodigoInserir
+            // 
+            this.lblCodigoInserir.AutoSize = true;
+            this.lblCodigoInserir.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCodigoInserir.Location = new System.Drawing.Point(805, 305);
+            this.lblCodigoInserir.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblCodigoInserir.Name = "lblCodigoInserir";
+            this.lblCodigoInserir.Size = new System.Drawing.Size(213, 41);
+            this.lblCodigoInserir.TabIndex = 27;
+            this.lblCodigoInserir.Text = "Inserir código";
+            // 
+            // tbxEmailCadastrado
+            // 
+            this.tbxEmailCadastrado.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.tbxEmailCadastrado.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbxEmailCadastrado.Location = new System.Drawing.Point(198, 357);
+            this.tbxEmailCadastrado.Margin = new System.Windows.Forms.Padding(4);
+            this.tbxEmailCadastrado.Multiline = true;
+            this.tbxEmailCadastrado.Name = "tbxEmailCadastrado";
+            this.tbxEmailCadastrado.Size = new System.Drawing.Size(459, 35);
+            this.tbxEmailCadastrado.TabIndex = 26;
+            // 
+            // lblEmailCadastrado
+            // 
+            this.lblEmailCadastrado.AutoSize = true;
+            this.lblEmailCadastrado.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmailCadastrado.Location = new System.Drawing.Point(291, 305);
+            this.lblEmailCadastrado.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblEmailCadastrado.Name = "lblEmailCadastrado";
+            this.lblEmailCadastrado.Size = new System.Drawing.Size(258, 41);
+            this.lblEmailCadastrado.TabIndex = 25;
+            this.lblEmailCadastrado.Text = "Email cadastrado";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1296, 653);
+            this.Controls.Add(this.pnlEsqueceu);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblCodigo);
+            this.Controls.Add(this.btnConfirmar);
+            this.Controls.Add(this.btnEnviar);
+            this.Controls.Add(this.tbxCodigoInserir);
+            this.Controls.Add(this.lblCodigoInserir);
+            this.Controls.Add(this.tbxEmailCadastrado);
+            this.Controls.Add(this.lblEmailCadastrado);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.pnlEsqueceu.ResumeLayout(false);
+            this.pnlEsqueceu.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxVoltar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFechar;
+        private System.Windows.Forms.Panel pnlEsqueceu;
+        private System.Windows.Forms.PictureBox pbxVoltar;
+        private System.Windows.Forms.Button btnMinimizar;
+        private System.Windows.Forms.Button btnMaximizar;
+        private System.Windows.Forms.Label lblSenha;
+        private System.Windows.Forms.Label lblEsqueceuSua;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblCodigo;
+        private System.Windows.Forms.Button btnConfirmar;
+        private System.Windows.Forms.Button btnEnviar;
+        private System.Windows.Forms.TextBox tbxCodigoInserir;
+        private System.Windows.Forms.Label lblCodigoInserir;
+        private System.Windows.Forms.TextBox tbxEmailCadastrado;
+        private System.Windows.Forms.Label lblEmailCadastrado;
+    }
+}
